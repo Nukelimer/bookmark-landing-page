94c07fa273aac215720382ac582f887541ae1f0e
codewithremi <codewithremi@gmail.com>
2023-03-03 22:18:36 +0100

    mobile first designed

--- style/tailwind.config.js
+++ style/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', '../index.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      Mantrope: ['Manrope', 'sans-serif'],
+      Golo: ['Golos Text', 'sans-serif'],
+    },
+    extend: {
+      boxShadow: {
+        boxShadow: ' 1px 1px 6px 1px rgba(50,50,53, 0.1)'
+      },
+      colors: {
+        'error-red': ' hsl(0, 94%, 66%)',
+        'soft-blue': 'hsl(231, 69%, 60%)',
+        'dark-blue-nav': ' hsl(229, 31%, 21%)',
+        'gray-blue': 'hsl(229, 8%, 60%)',
+      },
+    },
   },
   plugins: [],
 };
